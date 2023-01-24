a7781978ed6fc12e6c755cb7f7ecb8e94738f3ea
Carlos Infante <kuva5008@gmail.com>
2023-01-24 05:04:32 +0100

    database created ok

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db.init_app(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
