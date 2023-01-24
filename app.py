d1b13020cb92ccb25261751eec4c94d23be7409a
Carlos Infante <kuva5008@gmail.com>
2023-01-24 03:54:49 +0100

    href created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask"
+    return render_template('home.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
